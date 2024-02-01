866e29af08895c9efd75b4858a1aae853b6433a3
manishankar360 <manishankar.jaiswal@careers360.in>
2024-02-01 16:32:48 +0530

    library created

---
+++ kite_trade/__init__.py
@@ -0,0 +1,9 @@
+"""
+kite_trade.
+
+An python library for accessing kite zerodha API.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Manishankar Prasad Jaiswal'
+__credits__ = None
---
+++ kite_trade/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "kite_trade"
+__description__ = "The official Python client for the Kite Connect trading API"
+__url__ = "https://kite.trade"
+__download_url__ = "https://github.com/Manishankar152207/kite_trade.git"
+__version__ = "0.0.1"
+__author__ = "Manishankar"
+__author_email__ = "csmanishankar@gmail.com"
+__license__ = "MIT"
